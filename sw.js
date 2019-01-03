74750c4afcacaed7fe3ca4843a2567b1a6c170ba
Tiffany Jensen <39684784+tmj62113@users.noreply.github.com>
2019-01-03 10:48:03 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,63 @@
+var staticCacheName = 'restaurant-cache-1';
+
+let urlToCache = [
+	'/',
+	'/index.html',
+	'/restaurant.html',
+	'/css/styles.css',
+	'/css/lessthan850px.css',
+	'/css/lessthan560px.css',
+	'/js/dbhelper.js',
+	'/js/main.js',
+	'/js/restaurant_info.js',
+	'/data/restaurants.json', 
+	'/img/1.jpg', 
+	'/img/2.jpg',
+	'/img/3.jpg',
+	'/img/4.jpg',
+	'/img/5.jpg',
+	'/img/6.jpg',
+	'/img/7.jpg',
+	'/img/8.jpg',
+	'/img/9.jpg',
+	'/img/10.jpg',
+
+];
+self.addEventListener('install', function (event) {
+
+		event.waitUntil(
+			caches.open(staticCacheName).then(function (cache) {
+				console.log(cache);
+				return cache.addAll(urlToCache);
+
+			}).catch(error => {
+				console.log(error);
+			})
+		);
+});
+
+self.addEventListener('activate', function (event) {
+	event.waitUntil(
+		caches.keys().then(function (cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function (cacheName) {
+					return cacheName.startsWith('restaurant-') &&
+						cacheName !=staticCacheName;
+				}).map(function (cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+});
+
+self.addEventListener('fetch', function (event) {
+	event.respondWith(
+		caches.match(event.request).then(function (response) {
+			return response || fetch(event.request);
+		})
+	);	
+});
+
+
+
